7fe54631940bcc07edcae29ea72649f4865ccdfa
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-05-04 18:15:16 +0900

    setup lint and format

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,32 @@
-// const express=require("express")
-// const app=express()
-// const port=3000
-//
-// app.get("",(req,res)=>res.send("Hello World!"))
-//
-// app.listen(port,()=>{
-//     console.log(`Listening on http:localhost:${port}`)
-// })
+import express, { Request, Response, NextFunction } from "express";
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const hello = (name: string) => `Hello, ${name}!`;
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
-console.log(hello("TypeScript"));
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
+
+app.get("", (req, res) => res.send("Hello World!"));
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+const users: User[] = [
+  { id: 1, name: "User1", email: "user1@example.com" },
+  { id: 2, name: "User2", email: "user2@example.com" },
+  { id: 3, name: "User3", email: "user3@example.com" },
+];
+app.get("/users", (req: Request, res: Response) => {
+  res.send(JSON.stringify(users));
+});
